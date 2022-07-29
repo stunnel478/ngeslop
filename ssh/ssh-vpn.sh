321b57dd9f29b0cfea15013c3cab8a3c86bb40a6
stunnel478 <95111099+stunnel478@users.noreply.github.com>
2022-07-29 16:38:32 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -186,9 +186,9 @@ echo "/usr/sbin/nologin" >> /etc/shells
 
 # install squid (proxy nya aku matikan)
 cd
-#apt -y install squid3
-#wget -O /etc/squid/squid.conf "https://${akbarvpn}/squid3.conf"
-#sed -i $MYIP2 /etc/squid/squid.conf
+apt -y install squid
+wget -O /etc/squid/squid.conf "https://${akbarvpn}/squid3.conf"
+sed -i $MYIP2 /etc/squid/squid.conf
 
 # Install SSLH
 apt -y install sslh
@@ -374,7 +374,7 @@ cd /usr/bin
 wget -O addhost "https://${akbarvpn}/addhost.sh"
 wget -O slhost "https://${akbarvpn}/slhost.sh"
 wget -O about "https://${akbarvpn}/about.sh"
-wget -O menu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/menu.sh"
+wget -O menu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/menu.sh"
 wget -O addssh "https://${akbarvpn}/addssh.sh"
 wget -O trialssh "https://${akbarvpn}/trialssh.sh"
 wget -O delssh "https://${akbarvpn}/delssh.sh"
@@ -421,26 +421,26 @@ wget -O addtrgo "https://${akbarvpnnn}/addtrgo.sh"
 wget -O deltrgo "https://${akbarvpnnn}/deltrgo.sh"
 wget -O renewtrgo "https://${akbarvpnnn}/renewtrgo.sh"
 wget -O cektrgo "https://${akbarvpnnn}/cektrgo.sh"
-wget -O portsshnontls "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/websocket/portsshnontls.sh"
-wget -O portsshws "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/websocket/portsshws.sh"
+wget -O portsshnontls "https://raw.githubusercontent.com/stunnel478/ngeslop/main/websocket/portsshnontls.sh"
+wget -O portsshws "https://raw.githubusercontent.com/stunnel478/ngeslop/main/websocket/portsshws.sh"
 
-wget -O ipsaya "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/ipsaya.sh"
-wget -O sshovpnmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/sshovpn.sh"
-wget -O l2tpmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/l2tpmenu.sh"
-wget -O pptpmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/pptpmenu.sh"
-wget -O sstpmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/sstpmenu.sh"
-wget -O wgmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/wgmenu.sh"
-wget -O ssmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/ssmenu.sh"
-wget -O ssrmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/ssrmenu.sh"
-wget -O vmessmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/vmessmenu.sh"
-wget -O vlessmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/vlessmenu.sh"
+wget -O ipsaya "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/ipsaya.sh"
+wget -O sshovpnmenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/sshovpn.sh"
+wget -O l2tpmenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/l2tpmenu.sh"
+wget -O pptpmenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/pptpmenu.sh"
+wget -O sstpmenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/sstpmenu.sh"
+wget -O wgmenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/wgmenu.sh"
+wget -O ssmenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/ssmenu.sh"
+wget -O ssrmenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/ssrmenu.sh"
+wget -O vmessmenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/vmessmenu.sh"
+wget -O vlessmenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/vlessmenu.sh"
 wget -O grpcmenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/grpcmenu.sh"
-wget -O grpcupdate "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/grpcupdate.sh"
-wget -O trmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/trmenu.sh"
-wget -O trgomenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/trgomenu.sh"
-wget -O setmenu "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/setmenu.sh"
+wget -O grpcupdate "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/grpcupdate.sh"
+wget -O trmenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/trmenu.sh"
+wget -O trgomenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/trgomenu.sh"
+wget -O setmenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/setmenu.sh"
 wget -O slowdnsmenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/slowdnsmenu.sh"
-wget -O running "https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/update/running.sh"
+wget -O running "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/running.sh"
 wget -O updatemenu "https://raw.githubusercontent.com/stunnel478/ngeslop/main/update/updatemenu.sh"
 wget -O sl-fix "https://raw.githubusercontent.com/stunnel478/ngeslop/main/sslh-fix/sl-fix"
 
